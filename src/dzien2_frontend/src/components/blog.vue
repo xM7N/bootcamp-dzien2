abd7064267f5d53f23c506048e5774ef7ad79509
xM7N <steam.perr2@o2.pl>
2024-06-25 20:10:59 +0200

    added way to add new Wpis

---
+++ src/dzien2_frontend/src/components/blog.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <button @click="pobierzWpisy">refresh</button>
+        siema blog!
+        {{ wpisy }}
+        <input v-model="nowyBlog" type="text">
+        {{ nowyBlog }}
+        <button @click="dodajWpisy">Dodaj</button>
+    </div>
+</template>
+
+<script>
+import { dzien2_backend } from 'declarations/dzien2_backend/index';
+
+export default {
+    data() {
+        return {
+            wpisy: [],
+            nowyBlog: ""
+        }
+    },
+    methods: {
+        async dodajWpisy() {
+            await dzien2_backend.dodaj_wpis(this.nowyBlog);
+        }
+    },
+    async pobierzWpisy() {
+        this.wpisy = await dzien2_backend.odczytaj_wpisy();
+    }
+}
+
+</script>
