b71af8092ba73cfca34e50045556ee293d1564b1
xM7N <steam.perr2@o2.pl>
2024-06-26 17:37:35 +0200

    Added usun wpis and edytuj wpis

--- src/dzien2_backend/src/lib.rs
+++ src/dzien2_backend/src/lib.rs
@@ -12,7 +12,8 @@ fn greet(name: String, last_name: i8) -> String {
 #[ic_cdk::update]
 fn dodaj_wpis(wpis: String) {
     WPISY.with(|wpisy| {
-        wpisy.borrow_mut().push(wpis)
+        
+wpisy.borrow_mut().push(wpis)
     });
 }
 
@@ -21,4 +22,22 @@ fn odczytaj_wpisy() -> Vec<String> {
     WPISY.with(|wpisy| {
         wpisy.borrow().clone()
     })
+}
+
+#[ic_cdk::update]
+fn usun_wpis(id_wpisu: usize) {
+    WPISY.with(|wpisy: &RefCell<Vec<String>>| {
+        wpisy.borrow_mut().remove(id_wpisu)
+    });
+}
+
+
+#[ic_cdk::update]
+fn edytuj_wpis(id_wpisu: usize, nowy_wpis: String) {
+    WPISY.with(|wpisy: &RefCell<Vec<String>>| {
+        let mut binding = wpisy.borrow_mut();
+        let wpis = binding.get_mut(id_wpisu);
+        let stary_wpis = wpis.unwrap();
+        *stary_wpis = nowy_wpis;
+    });
 }
