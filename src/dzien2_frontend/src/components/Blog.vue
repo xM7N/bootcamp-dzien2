0dd97a224e8d6ccaae07ffae7bcfea1a4c52d826
xM7N <steam.perr2@o2.pl>
2024-06-26 19:29:37 +0200

    visual changes

--- src/dzien2_frontend/src/components/Blog.vue
+++ src/dzien2_frontend/src/components/Blog.vue
@@ -1,10 +1,22 @@
 <template>
     <div>
-        <button @click="pobierzWpisy">refresh</button>
-        siema blog!
-        {{ wpisy }}
-        <input v-model="nowyBlog" type="text">
-        <button @click="dodajWpisy">dodaj</button>
+        <h2 class="text-blue-600 ">Wpisy na bloga: </h2>
+        <div class="w-100% flex flex-row-reverse">
+            <button @click="pobierzWpisy" class="bg-blue-600 rounded text-white p-4">refresh</button>
+        </div>
+
+        <br>
+        <div class="grid mx-6 gap-4">
+            <div v-for="(index, wpis) in wpisy" class="drop-shadow-x1 bg-stone-300 p-4">
+                <p>{{ wpis }}</p>
+            </div>
+        </div>
+
+        <div class="flex justify-center flex-col">
+            <input v-model="nowyBlog" type="text" class="border-2 border-blue-600 p-4">
+            <button @click="dodajWpisy" class="bg-blue-600 rounded text-white p-4">dodaj</button>
+        </div>
+
     </div>
 </template>
 
@@ -26,6 +38,8 @@ export default {
             this.wpisy = await dzien2_backend.odczytaj_wpisy();
         }
     },
-
+    async mounted() {
+        this.pobierzWpisy()
+    }
 }
 </script>
